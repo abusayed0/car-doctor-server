a7e8b7995dab6eb1530093421ee075e5aceff4a7
abusayed0 <abusayed48876@gmail.com>
2023-11-05 18:41:57 +0600

    created get user bookings, delete a booking, update a booking api

--- index.js
+++ index.js
@@ -52,20 +52,74 @@ async function run() {
             res.send(service)
         });
 
+        // get user bookings 
+        app.get("/bookings", async (req, res) => {
+            const userEmail = req.query.email;
+            // console.log(userEmail);
+
+
+            const query = { email: userEmail };
+            const options = {
+                sort: { title: 1 },
+                projection: { _id: 0, title: 1, imdb: 1 },
+            };
+
+            const cursor = bookings.find(query);
+            const userBooking = await cursor.toArray();
+            res.send(userBooking);
+        });
+
         // __________________________________
 
 
         // all post request 
         // _____________________________________
-        
+
         // post a booking 
         app.post("/bookings", async (req, res) => {
             const data = req.body;
             const result = await bookings.insertOne(data);
             res.send(result);
         });
-        
+
         // ________________________________________
+
+
+        // all delete api 
+        // _________________________________
+
+        // delete single booking 
+        app.delete("/bookings/:id", async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await bookings.deleteOne(query);
+            res.send(result);
+        });
+
+        // _________________________________
+
+
+        // all putch api 
+        // _______________________________________
+
+        // update single booking 
+        app.patch("/bookings/:id", async(req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            console.log(data);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: data.status
+                },
+            };
+            const result = await bookings.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+
+        // _______________________________________
 
 
 
